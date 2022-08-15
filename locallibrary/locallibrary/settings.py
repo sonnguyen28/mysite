78841aaef273f4e019635e1d28e82e91aba85932
sonnguyen28 <hoangson280800@gmail.com>
2022-08-15 16:18:31 +0700

    Django Tutorial Part 5: Creating our home page

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -133,6 +133,22 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
