cfd55c3ed1b34cbd903e2955f68d8814a8a1ec3e
sonnguyen28 <hoangson280800@gmail.com>
2022-08-15 10:36:29 +0700

    Django Tutorial Part 4: Django admin site

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.urls import reverse
-import uuid
-from datetime import date
 from django.contrib.auth.models import User
-
+from django.utils.translation import gettext_lazy as _
+from datetime import date
+import uuid
 class Genre(models.Model):
     name = models.CharField(
         max_length=200,
-        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+        help_text=_("Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
     )
 
     def __str__(self):
@@ -16,7 +16,7 @@ class Genre(models.Model):
 
 class Language(models.Model):
     name = models.CharField(max_length=200,
-                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+                            help_text=_("Enter the book's natural language (e.g. English, French, Japanese etc.)"))
 
     def __str__(self):
         return self.name
@@ -25,12 +25,12 @@ class Language(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    summary = models.TextField(max_length=1000, help_text=_("Enter a brief description of the book"))
     isbn = models.CharField('ISBN', max_length=13,
                             unique=True,
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
                                       '">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    genre = models.ManyToManyField(Genre, help_text=_("Select a genre for this book"))
 
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
@@ -50,7 +50,7 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                          help_text="Unique ID for this particular book across whole library")
+                          help_text=_("Unique ID for this particular book across whole library"))
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -72,7 +72,7 @@ class BookInstance(models.Model):
         choices=LOAN_STATUS,
         blank=True,
         default='d',
-        help_text='Book availability')
+        help_text=_('Book availability'))
 
     class Meta:
         ordering = ['due_back']
